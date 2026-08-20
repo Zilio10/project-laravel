2ff88bc4bc6c06693ce34b6ba637b2c1e0d9dc36
Zilio - Desktop <henriquezilio4@gmail.com>
2026-08-20 15:42:53 -0300

    post

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index()
     {
-
-        $events = Event::all();
+        $events = Event::all(); // Método para pegar todos os dados da tabela no banco
 
         return view('welcome', ['events' => $events]);
     }
@@ -17,5 +17,32 @@ class EventController extends Controller
     public function create()
     {
         return view('events.create');
+    }
+
+    public function store(Request $request)
+    {
+        $event = new Event;
+
+        $event->title = $request->title;
+        $event->city = $request->city;
+        $event->private = $request->private;
+        $event->description = $request->description;
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $request->image->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+
+        }
+
+        $event->save(); // Método para salvar informações no banco
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso');
     }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,6 +3,32 @@
 @section('title', 'Criar evento')
 @section('content')
 
-    <h1>Crie um evento</h1>
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="image">Imagem do evento</label>
+                <input type="file" class="form-control" id="image" name="image">
+
+                <label for="title">Evento</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+
+                <label for="city">Cidade:</label>
+                <input type="city" class="form-control" id="city" name="city" placeholder="Nome da cidade">
+
+                <label for="private">O evento é privado?</label>
+                <select name="private" id="provate" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+
+                <label for="description">Descrição</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que acontecerá neste evento?"></textarea>
+
+                <input type="submit" class="btn btn-primary" value="Criar evento">
+            </div>
+        </form>
+    </div>
 
 @endsection
