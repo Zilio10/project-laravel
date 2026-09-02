23d42e35817d483ec58acf7d4210573c607878b1
Zilio - Desktop <henriquezilio4@gmail.com>
2026-09-02 13:28:01 -0300

    auth

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="/css/styles.css">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 
 </head>
 
@@ -40,17 +41,35 @@
                             </a>
                         </li>
 
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">
-                                Entrar
-                            </a>
-                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a href="/dashboard" class="nav-link">
+                                    Meus eventos
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">
-                                Cadastrar
-                            </a>
-                        </li>
+                            <li class="nav-item">
+                                <form action="/logout" method="POST">
+                                    @csrf
+                                    <a href="/logout" class="nav-link" onclick="event.preventDefault(); this.closest('form').submit();">Sair</a>
+                                </form>
+                            </li>
+                        @endauth
+
+                        @guest
+                            <li class="nav-item">
+                                <a href="/login" class="nav-link">
+                                    Entrar
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="/register" class="nav-link">
+                                    Cadastrar
+                                </a>
+                            </li>
+                        @endguest
+
                     </ul>
                 </div>
 
@@ -62,7 +81,7 @@
     <main>
         <div class="container-fluid">
             <div class="row">
-                @if(session('msg'))
+                @if (session('msg'))
                     <p class="msg">{{ session('msg') }}</p>
                 @endif
                 @yield('content')
